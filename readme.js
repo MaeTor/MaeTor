4f0264e03b37bded8c6b78154965adbda38eecf0
TORREGROSA Mael <mael.torregrosa@etu.univ-amu.fr>
2024-08-16 13:00:00 +0200

    fix file structure and line number because difference in the structure or the text around the placeholder will prevent the script from finding the correct line and updating it.

--- readme.js
+++ readme.js
@@ -1,48 +1,34 @@
-module.exports = `
-<p align="center">
+module.exports = `<p align="center">
   <img src="https://capsule-render.vercel.app/api?type=waving&height=200&color=gradient&customColorList=30&text=Welcome%20To%20MaeTor's%20Github%20!%20ᓚᘏᗢ%20👨🏻‍💻&fontAlign=50&fontAlignY=25&fontSize=45&desc=Feel%20Free%20To%20Navigate%20Through%20My%20Projects%20🎉&animation=scaleIn&descSize=35" style="width: 100%;"/>
 </p>
-`;
 
-module.exports += `
 ## Langages and Frameworks
 | Langages | Frameworks |
 | --- | --- |
 | ![Python](https://img.icons8.com/color/48/000000/python.png) Python | ![Django](https://img.icons8.com/color/48/000000/django.png) Django |
 | ![php](https://img.icons8.com/?size=47&id=fAMVO_fuoOuC&format=png&color=000000) PHP | ![Flask](https://img.icons8.com/color/48/000000/flask.png) Flask |
-`;
-
-module.exports += `
 | ![Flask](https://img.icons8.com/?size=45&id=uJM6fQYqDaZK&format=png&color=000000.png) Typescript | ![Laravel](https://img.icons8.com/?size=40&id=lRjcvhvtR81o&format=png&color=000000) Laravel |
-`;
-
-module.exports += `
 | ![Java](https://img.icons8.com/?size=48&id=lTKW3iI3wIT0&format=png&color=0000000) Java | ![Symfony](https://img.icons8.com/?size=40&id=eXY8XFlpbTUt&format=png&color=000000) Symfony |
-`;
 
-module.exports += `
+
 ## Operating Systems
 |   Linux   |   Windows  |    Ubuntu  |  Kali  |  ParrotOS  |
 |:---------:|:----------:|:----------:|:----------:|:---------:|
-`;
-module.exports += `
+
+
 | <img src="https://github.com/devicons/devicon/blob/master/icons/linux/linux-original.svg" title="Linux" alt="Linux" width="55" height="55"/> 
 | <img src="https://github.com/devicons/devicon/blob/master/icons/windows11/windows11-original-wordmark.svg" title="Windows" alt="Windows" width="55" height="55"/> 
 | <img src="https://github.com/devicons/devicon/blob/master/icons/ubuntu/ubuntu-original.svg" title="Ubuntu" alt="Ubuntu" width="55" height="55"/> 
-| <img src="https://github.com/canaleal/devicon/blob/new-icon-kali-linux/icons/kalilinux/kalilinux-original-wordmark.svg" title="Kali Linux" alt="Kali Linux" width="55" height="55"/> 
+| <img src="https://github.com/canaleal/devicon/blob/new-icon-kali-linux/icons/kalilinux-original-wordmark.svg" title="Kali Linux" alt="Kali Linux" width="55" height="55"/> 
 | <img src="https://github.com/ParrotSec/parrot-themes/blob/master/icons/parrot-logo.png" title="ParrotOS" alt="ParrotOS" width="55" height="55"/>
-`;
+|
 
-module.exports += `
 Last update on <#today_date>
-`;
 
-module.exports += `
-<#days_before_new_years> days before new years
-`;
+<#days_before_new_years> days before new year
 
-module.exports += `
 <#rico_signing>
 `;
+
 
 
