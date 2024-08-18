12415f0778be34c20df39613097f8e72f83dbe47
TORREGROSA Mael <mael.torregrosa@etu.univ-amu.fr>
2024-08-18 12:30:00 +0200

    add Languages and Frameworks section

--- readme.js
+++ readme.js
@@ -3,3 +3,11 @@ module.exports = `
   <img src="https://capsule-render.vercel.app/api?type=waving&height=200&color=gradient&customColorList=30&text=Welcome%20To%20MaeTor's%20Github%20!%20ᓚᘏᗢ%20👨🏻‍💻&fontAlign=50&fontAlignY=25&fontSize=45&desc=Feel%20Free%20To%20Navigate%20Through%20My%20Projects%20🎉&animation=scaleIn&descSize=35" style="width: 100%;"/>
 </p>
 `;
+
+module.exports += `
+## Langages and Frameworks
+| Langages | Frameworks |
+| --- | --- |
+| ![Python](https://img.icons8.com/color/48/000000/python.png) Python | ![Django](https://img.icons8.com/color/48/000000/django.png) Django |
+| ![php](https://img.icons8.com/?size=47&id=fAMVO_fuoOuC&format=png&color=000000) PHP | ![Flask](https://img.icons8.com/color/48/000000/flask.png) Flask |
+`;
